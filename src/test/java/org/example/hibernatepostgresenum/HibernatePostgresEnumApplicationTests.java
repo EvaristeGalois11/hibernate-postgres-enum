12eb1f7750f58e8489607ef9aee6bdc6c342cd45
Claudio Nave <evaristegalois11@proton.me>
2024-05-16 22:59:20 +0200

    Convert to testcontainers

--- src/test/java/org/example/hibernatepostgresenum/HibernatePostgresEnumApplicationTests.java
+++ src/test/java/org/example/hibernatepostgresenum/HibernatePostgresEnumApplicationTests.java
@@ -1,13 +1,22 @@
 package org.example.hibernatepostgresenum;
 
+import org.example.hibernatepostgresenum.constant.FooBar;
+import org.example.hibernatepostgresenum.entity.Dummy;
+import org.example.hibernatepostgresenum.repository.DummyRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.datasource.url=jdbc:tc:postgresql:16:///test")
 class HibernatePostgresEnumApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Autowired private DummyRepository dummyRepository;
 
+  @Test
+  void insertDummy() {
+    var dummy = new Dummy();
+    dummy.setStatus(FooBar.FOO);
+    dummyRepository.save(dummy);
+  }
 }
